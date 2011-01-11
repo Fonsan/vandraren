c64b3a4c0aa6dddf9cd15b494b9443f5033cd429
Fonsan <fonsan@gmail.com>
2011-01-11 16:54:05 +0100

    Lade till lite defaults

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+User.create! "email"=>"fonsan@gmail.com","password" => "mariner2",  "username"=>"Admin"
